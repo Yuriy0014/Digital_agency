341d428037e6123e74f632f7d810b81afa6fb798
Yuriy_log <ylogachev-sfedu@mail.ru>
2020-12-30 06:06:28 +0300

    Added draft JS slider

--- js/main.js
+++ js/main.js
@@ -1,3 +1,27 @@
-$(function(){
+$(function() {
+	$('.top-slider').slick({
+		dots: true,
+		infinite: false,
+		slidesToScroll: 1,
+		responsive: [
+			{
+				breakpoint: 1024,
+				settings: {
 
+				}
+			},
+			{
+				breakpoint: 600,
+				settings: {
+
+				}
+			},
+			{
+				breakpoint: 480,
+				settings: {
+
+				}
+			}
+		]
+	});
 });
