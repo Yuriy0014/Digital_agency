a03bb7566abe1c53f8b4d8da115d7442722b89bf
Yuriy_log <ylogachev-sfedu@mail.ru>
2021-01-22 22:42:35 +0300

    Added arrows for slider + modified styles for top-slider item,title

--- js/main.js
+++ js/main.js
@@ -3,6 +3,8 @@ $(function () {
 		dots: true,
 		infinite: false,
 		slidesToScroll: 1,
+		nextArrow: '<button type="button" class="slick-arrow slick-next"><img src="images/next-arrow.svg" alt="next arrow"></button>',
+		prevArrow: '<button type="button" class="slick-arrow slick-prev"><img src="images/prev-arrow.svg" alt="previous arrow"></button>',
 		responsive: [
 			{
 				breakpoint: 1024,
